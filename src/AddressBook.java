63e5f744443345c215b6ae8b6b573f23f928adb2
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-08-24 19:25:24 +0530

    creating contacts for address book

---
+++ src/AddressBook.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+creating Address book
+ */
+public class AddressBook {
+    static Scanner sc = new Scanner(System.in);
+
+    ArrayList<Contacts> list = new ArrayList<>();
+    /*
+    creating address book contact details list
+     */
+    public void contactList() {
+        System.out.println("Enter First Name");
+        String firstName = sc.nextLine();
+
+        System.out.println("Enter last Name");
+        String lastName = sc.nextLine();
+
+        System.out.println("Enter State");
+        String state = sc.nextLine();
+
+        System.out.println("Enter City");
+        String city = sc.nextLine();
+
+        System.out.println("Enter Address");
+        String address = sc.nextLine();
+
+        System.out.println("Enter phone number");
+        String phone = sc.nextLine();
+
+        System.out.println("Enter email");
+        String email = sc.nextLine();
+
+        Contacts cList = new Contacts(firstName, lastName, address, city, state, phone, email);
+    /*
+    using add method for adding data in array list
+     */
+        list.add(cList);
+
+    }
+}
---
+++ src/AddressBookMain.java
@@ -0,0 +1,16 @@
+/**
+ * This is address book program
+ * here caaling the methods and class for output
+ */
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Main Program");
+
+        AddressBook a1 = new AddressBook();
+        a1.contactList();
+        System.out.println(a1.list);
+
+
+    }
+
+    }

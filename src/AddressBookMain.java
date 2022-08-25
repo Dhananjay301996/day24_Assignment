d7e6d7c5d38122974d7af5055f3aa9ab8303df2b
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-08-25 17:13:52 +0530

    Ability to delete contact

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -10,25 +10,26 @@ public class AddressBookMain {
         a1.contactList();
         a1.contactList();
         System.out.println(a1.list);
-
+    /*
+    delete the contact
+     */
         System.out.println("if you want to change any data of a person, reply y/n : ");
         String check = AddressBook.sc.nextLine();
 
-        System.out.println("enter name whose details you want to edit : ");
-        String name = AddressBook.sc.nextLine();
-
         if (check.equals("y")) {
+            System.out.println("enter name whose details you want to remove : ");
+            String name = AddressBook.sc.nextLine();
+
+            int i =0;
             for (Contacts cThrough : a1.list) {
 
                 if (cThrough.getFirstName().equals(name)) {
-                    System.out.println("enter updated phone number : ");
-                    cThrough.setPhone(AddressBook.sc.next());
+                    a1.list.remove(i);
                     break;
                 }
-
+                i++;
             }
         }
-
         System.out.println(a1.list);
 
 
